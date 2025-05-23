cc5cadc26c18374da154b29c4643ac491cc97a48
MatheusNicknig <160255395+MatheusNicknig@users.noreply.github.com>
2025-05-23 10:16:26 -0300

    ex5.py

--- ex5.py
+++ ex5.py
@@ -1,3 +1,4 @@
+#Peça ao usuário para digitar 3 notas, se a média do aluno for maior ou igual a 7 o aluno foi aprovado, se não reprovado
 nota1 = int(input('Digite a primeira nota: '))
 nota2 = int(input('Digite a segunda nota: '))
 nota3 = int(input('Digite a terceira nota: '))
