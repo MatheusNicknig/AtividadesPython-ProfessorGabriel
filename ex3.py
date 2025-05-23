8f9e281c636e6654b9f05d926e9c248843bff34c
MatheusNicknig <160255395+MatheusNicknig@users.noreply.github.com>
2025-05-23 10:12:50 -0300

    ex3.py

--- ex3.py
+++ ex3.py
@@ -1,4 +1,5 @@
-letra = str(input('Digite uma letra: '))
+#Peça ao usuário para digitar uma letra e verifique se ela é uma vogal ou consoante
+letra = char(input('Digite uma letra: '))
 if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
     print('A letra é uma vogal')
 else:
