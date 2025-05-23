d5e4d9bd58b22a69fb39f82d3db6446313708018
MatheusNicknig <160255395+MatheusNicknig@users.noreply.github.com>
2025-05-23 10:14:13 -0300

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -1,3 +1,4 @@
+#Peça ao usuário para digitar uma sena e para confirma-lá, se a senha for igual a confirmação acesso permitido, se não acesso negado
 senha = str(input('Digite uma senha: '))
 confirmação = str(input('Confirme a senha: '))
 if confirmação == senha:
