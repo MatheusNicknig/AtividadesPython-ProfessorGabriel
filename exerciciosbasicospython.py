692d8dc9470e36dd705f40d664e5fa07a66bb266
MatheusNicknig <160255395+MatheusNicknig@users.noreply.github.com>
2025-10-03 11:16:42 -0300

    exerciciosbasicospython.py
    Atualização

--- exerciciosbasicospython.py
+++ exerciciosbasicospython.py
@@ -38,79 +38,79 @@
 # Entre 12 e 17 anos → Adolescente
 # 18 anos ou mais → Adulto
 
-idade = int(input('Informe a sua idade: '))
-if idade < 12 and idade >= 1:
-    print(f'Criança')
-elif idade >= 12 and idade <= 17:
-    print(f'Adolescente')
-elif idade > 18 and idade <= 100:
-    print(f'Adulto')
-else:
-    print(f'Idade inválida')
+def classificar_por_idade(idade):
+    if idade < 12 and idade >-1:
+        return 'Criança'
+    elif idade >=12 and idade <=17:
+        return 'Adolescente'
+    elif idade >= 18 and idade <=100:
+        return 'Adulto'
+    else:
+        return 'Idade Inválida'
 
 
 # 2. Maior de Dois Números
 # Solicite dois números ao usuário e exiba o maior deles. Caso sejam iguais, informe isso.
 
-numero = int(input('Informe um número: '))
-numero2 = int(input('Informe um outro número: '))
+#numero = int(input('Informe um número: '))
+#numero2 = int(input('Informe um outro número: '))
 
-if numero > numero2:
-    print(f'{numero} é maior que {numero2}')
-elif numero == numero2:
-    print(f'{numero} é igual a {numero2}')
-else:
-    print(f'{numero} é menor que {numero2}')
+#if numero > numero2:
+#    print(f'{numero} é maior que {numero2}')
+#elif numero == numero2:
+#    print(f'{numero} é igual a {numero2}')
+#else:
+#    print(f'{numero} é menor que {numero2}')
 
 
 # 3. Verificação de Vogal ou Consoante
 # Peça ao usuário para digitar uma letra e informe se é uma vogal (a, e, i, o, u) ou consoante.
 
-letra = input('Digite uma letra: ')
+#letra = input('Digite uma letra: ')
 
-if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-    print(f'A letra digitada é uma vogal.')
-else:
-    print(f'A letra digitada é uma consoante.')
+#if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+#    print(f'A letra digitada é uma vogal.')
+#else:
+#    print(f'A letra digitada é uma consoante.')
 
 
 # 4. Comparação de Senhas
 # Solicite ao usuário que defina uma senha e, em seguida, peça para confirmá-la. Caso as senhas sejam iguais, exiba “Acesso permitido”, senão, exiba “Senhas não coincidem”.
 
-senha1 = input('Digite uma senha: ')
+#senha1 = input('Digite uma senha: ')
 
-senhaConfirmada = input('Confirme a sua senha: ')
+#senhaConfirmada = input('Confirme a sua senha: ')
 
-if(senha1 == senhaConfirmada):
-    print('Acesso permitido!')
-else:
-    print('Acesso negado!')
+#if(senha1 == senhaConfirmada):
+#    print('Acesso permitido!')
+#else:
+#    print('Acesso negado!')
 
 # 5. Cálculo de Média e Aprovação
 # Peça ao usuário para digitar três notas e calcule a média. Se a média for maior ou igual a 7, o aluno está aprovado, caso contrário, está reprovado.
 
-nota1 = float(input('Digite a primera nota: '))
-nota2 = float(input('Digite a segunda nota: '))
-nota3 = float(input('Digite a terceira nota: '))
+#nota1 = float(input('Digite a primera nota: '))
+#nota2 = float(input('Digite a segunda nota: '))
+#nota3 = float(input('Digite a terceira nota: '))
 
-media = (nota1 + nota2 + nota3) / 3
+#media = (nota1 + nota2 + nota3) / 3
 
-if media >= 7 and media <= 10:
-    print(f'O aluno está aprovado com média: {media}!')
-elif media < 7 and media >= 0:
-    print(f'O aluno está reprovado com média: {media}!')
-else:
-    print('Média inválida!')
+#if media >= 7 and media <= 10:
+#    print(f'O aluno está aprovado com média: {media}!')
+#elif media < 7 and media >= 0:
+#    print(f'O aluno está reprovado com média: {media}!')
+#else:
+#    print('Média inválida!')
 
 # 6. Escreva um programa que leia 3 números inteiros e imprima na tela os valores em ordem decrescente
 
-numero1 = int(input('Digite um primeiro numéro inteiro: '))
-numero2 = int(input('Digite um segundo numéro inteiro: '))
-numero3 = int(input('Digite um terceiro numéro inteiro: '))
+#numero1 = int(input('Digite um primeiro numéro inteiro: '))
+#numero2 = int(input('Digite um segundo numéro inteiro: '))
+#numero3 = int(input('Digite um terceiro numéro inteiro: '))
 
-listaDeNumeros = [numero1, numero2, numero3]
+#listaDeNumeros = [numero1, numero2, numero3]
 
-print(f'A lista dos números em ordem decrescente é: {sorted(listaDeNumeros,reverse=True)}')
+#print(f'A lista dos números em ordem decrescente é: {sorted(listaDeNumeros,reverse=True)}')
 
 
 # 7. Efetuar o cálculo da quantidade de litros de combustível gasta em uma viagem, utilizando um automóvel que faz
@@ -120,11 +120,11 @@ print(f'A lista dos números em ordem decrescente é: {sorted(listaDeNumeros,rev
 # 	fórmula: LITROS_USADOS = DISTANCIA / 12. O programa deve apresentar os valores da velocidade média,
 # 	tempo gasto, a distância percorrida e a quantidade de litros utilizada na viagem. Dica: trabalhe com valores reais.
 
-tempoGasto = int(input('Digite o valor do tempo gasto em horas: '))
-velocidadeMedia = int(input('Digite o valor da velocidade média em KM/H: '))
+#tempoGasto = int(input('Digite o valor do tempo gasto em horas: '))
+#velocidadeMedia = int(input('Digite o valor da velocidade média em KM/H: '))
 
-distancia = tempoGasto * velocidadeMedia
+#distancia = tempoGasto * velocidadeMedia
 
-litrosUsados = distancia / 12
+#litrosUsados = distancia / 12
 
-print(f'O valor da velocidade média é de: {velocidadeMedia}KM/H \nO valor do tempo gasto é de: {tempoGasto} Horas \nO valor da distância percorrida é de: {distancia}KM \nA quantidade de litros usados na viagem foi de: {litrosUsados}L')
+#print(f'O valor da velocidade média é de: {velocidadeMedia}KM/H \nO valor do tempo gasto é de: {tempoGasto} Horas \nO valor da distância percorrida é de: {distancia}KM \nA quantidade de litros usados na viagem foi de: {litrosUsados}L')
