1eeb6fadbd1b0d1ded7a0b98f2433a0a2a2e9d24
MatheusNicknig <160255395+MatheusNicknig@users.noreply.github.com>
2025-10-10 10:50:22 -0300

    Add files via upload

--- exerciciosbasicospython.py
+++ exerciciosbasicospython.py
@@ -38,72 +38,74 @@
 # Entre 12 e 17 anos → Adolescente
 # 18 anos ou mais → Adulto
 
-def classificar_por_idade(idade):
-    if idade < 12 and idade >-1:
-        return 'Criança'
-    elif idade >=12 and idade <=17:
-        return 'Adolescente'
-    elif idade >= 18 and idade <=100:
-        return 'Adulto'
-    else:
-        return 'Idade Inválida'
+#def classificar_faixa_etaria(idade):
+#   if idade < 12 and idade >= 1:
+#        return 'Criança'
+#   elif idade >= 12 and idade <= 17:
+#        return 'Adolescente'
+#    elif idade >= 18 and idade <= 100:
+#        return 'Adulto'
+#    else:
+#        return 'Idade inválida'
 
 
 # 2. Maior de Dois Números
 # Solicite dois números ao usuário e exiba o maior deles. Caso sejam iguais, informe isso.
+#def comparar_dois_numeros(num1, num2):
+#    if num1 > num2:
+#        return f'{num1} é maior que {num2}'
+#    elif num1 == num2:
+#       return f'{num1} é igual a {num2}'
+#    else:
+#        return f'{num1} é menor que {num2}'
 
-#numero = int(input('Informe um número: '))
-#numero2 = int(input('Informe um outro número: '))
-
-#if numero > numero2:
-#    print(f'{numero} é maior que {numero2}')
-#elif numero == numero2:
-#    print(f'{numero} é igual a {numero2}')
-#else:
-#    print(f'{numero} é menor que {numero2}')
 
 
 # 3. Verificação de Vogal ou Consoante
 # Peça ao usuário para digitar uma letra e informe se é uma vogal (a, e, i, o, u) ou consoante.
+#def verificar_vogal_ou_consoante(letra):
+#    if letra in 'aeiouAEIOU':
+#        return 'A letra digitada é uma vogal.'
+#    else:
+#        return 'A letra digitada é uma consoante.'
 
-#letra = input('Digite uma letra: ')
-
-#if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
-#    print(f'A letra digitada é uma vogal.')
-#else:
-#    print(f'A letra digitada é uma consoante.')
 
 
 # 4. Comparação de Senhas
 # Solicite ao usuário que defina uma senha e, em seguida, peça para confirmá-la. Caso as senhas sejam iguais, exiba “Acesso permitido”, senão, exiba “Senhas não coincidem”.
-
-#senha1 = input('Digite uma senha: ')
-
-#senhaConfirmada = input('Confirme a sua senha: ')
-
-#if(senha1 == senhaConfirmada):
-#    print('Acesso permitido!')
-#else:
-#    print('Acesso negado!')
+#def comparacao_de_senhas(senha1, senha2):
+#    if senha1 == senha2:
+#        return 'Acesso permitido!'
+#    else:
+#        return 'Acesso negado!'
 
 # 5. Cálculo de Média e Aprovação
 # Peça ao usuário para digitar três notas e calcule a média. Se a média for maior ou igual a 7, o aluno está aprovado, caso contrário, está reprovado.
-
+#def calcular_media(nota1, nota2, nota3):
+#    media = (nota1 + nota2 + nota3) / 3
+#    if media >= 7 and media <= 10:
+#        return f'O aluno está aprovado com média: {media}!'
+#    elif media < 7 and media >= 0:
+#        return f'O aluno está reprovado com média: {media}!'
+#    else:
+#        return 'Média inválida!'
 #nota1 = float(input('Digite a primera nota: '))
-#nota2 = float(input('Digite a segunda nota: '))
+#ota2 = float(input('Digite a segunda nota: '))
 #nota3 = float(input('Digite a terceira nota: '))
 
 #media = (nota1 + nota2 + nota3) / 3
 
 #if media >= 7 and media <= 10:
-#    print(f'O aluno está aprovado com média: {media}!')
+ #   print(f'O aluno está aprovado com média: {media}!')
 #elif media < 7 and media >= 0:
-#    print(f'O aluno está reprovado com média: {media}!')
+ #   print(f'O aluno está reprovado com média: {media}!')
 #else:
-#    print('Média inválida!')
+ #   print('Média inválida!')
 
 # 6. Escreva um programa que leia 3 números inteiros e imprima na tela os valores em ordem decrescente
-
+#def ordenar_numeros_decrescente(num1, num2, num3):
+#    listaDeNumeros = [num1, num2, num3]
+#    return sorted(listaDeNumeros, reverse=True)
 #numero1 = int(input('Digite um primeiro numéro inteiro: '))
 #numero2 = int(input('Digite um segundo numéro inteiro: '))
 #numero3 = int(input('Digite um terceiro numéro inteiro: '))
@@ -119,7 +121,11 @@ def classificar_por_idade(idade):
 # 	Tendo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem com a
 # 	fórmula: LITROS_USADOS = DISTANCIA / 12. O programa deve apresentar os valores da velocidade média,
 # 	tempo gasto, a distância percorrida e a quantidade de litros utilizada na viagem. Dica: trabalhe com valores reais.
-
+def calcular_litros_usados(tempoGasto, velocidadeMedia):
+    distancia = tempoGasto * velocidadeMedia
+    litrosUsados = distancia / 12
+    return litrosUsados
+    
 #tempoGasto = int(input('Digite o valor do tempo gasto em horas: '))
 #velocidadeMedia = int(input('Digite o valor da velocidade média em KM/H: '))
 
--- testeunitario_MN.py
+++ testeunitario_MN.py
@@ -1,25 +1,58 @@
 import unittest
 
 from exerciciosbasicospython import *
+
 class TesteCompleto(unittest.TestCase):
-    def testar_se_numero_eh_par(self):
-        numero_a_ser_testado = 18
-        self.assertTrue(numero_a_ser_testado % 2 == 0)
+    #def testar_se_numero_eh_par(self):
+     #   numero_a_ser_testado = 14
+     #   self.assertTrue(numero_a_ser_testado % 2 == 0)
 
-    def testar_se_numero_eh_impar(self):
-        numero_a_ser_testado = 17
-        self.assertTrue(numero_a_ser_testado % 2 == 1)
+   # def testar_se_numero_eh_impar(self):
+       # numero_a_ser_testado = 11
+       # self.assertTrue(numero_a_ser_testado % 2 == 1)
 
-    def testar_conta(self):
-        resultado_conta = 15 + 5 + 25 + 7
-        self.assertEqual(resultado_conta, 52)
+    #def testar_conta(self):
+       # resultado_conta = 10 + 5 
+       # self.assertEqual(resultado_conta, 15)
 
-    def testar_classificar_por_idade(self):
-        self.assertEqual(classificar_por_idade(10), 'Criança')
-        self.assertEqual(classificar_por_idade(15), 'Adolescente')
-        self.assertEqual(classificar_por_idade(25), 'Adulto')
-        self.assertEqual(classificar_por_idade(-5), 'Idade Inválida')
-        self.assertEqual(classificar_por_idade(150), 'Idade Inválida')
+    #def testar_classificar_faixa_etaria(self):
+        #self.assertEqual(classificar_faixa_etaria(10), 'Criança')
+        #self.assertEqual(classificar_faixa_etaria(15), 'Adolescente')
+        #self.assertEqual(classificar_faixa_etaria(25), 'Adulto')
+        #self.assertEqual(classificar_faixa_etaria(-5), 'Idade inválida')
+        #self.assertEqual(classificar_faixa_etaria(150), 'Idade inválida')
 
+  #def testar_comparar_dois_numeros(self):
+        #self.assertEqual(comparar_dois_numeros(10, 5), '10 é maior que 5')
+        #self.assertEqual(comparar_dois_numeros(5, 10), '5 é menor que 10')
+        #self.assertEqual(comparar_dois_numeros(7, 7), '7 é igual a 7')
+
+    #def testar_verificar_vogal_ou_consoante(self):
+        #self.assertEqual(verificar_vogal_ou_consoante('a'), 'A letra digitada é uma vogal.')
+        #self.assertEqual(verificar_vogal_ou_consoante('b'), 'A letra digitada é uma consoante.')
+        #self.assertEqual(verificar_vogal_ou_consoante('E'), 'A letra digitada é uma vogal.')
+        #self.assertEqual(verificar_vogal_ou_consoante('Z'), 'A letra digitada é uma consoante.')
+
+  #def test_comparacao_de_senhas(self):
+        #self.assertEqual(comparacao_de_senhas('senha123', 'senha123'), 'Acesso permitido!')
+        #self.assertEqual(comparacao_de_senhas('senha123', 'senha456'), 'Acesso negado!')
+
+    #def teste_calcular_media(self):
+        #self.assertEqual(calcular_media(8, 7, 9), 'O aluno está aprovado com média: 8.0!')
+        #self.assertEqual(calcular_media(5, 6, 4), 'O aluno está reprovado com média: 5.0!')
+        #self.assertEqual(calcular_media(10, 10, 10), 'O aluno está aprovado com média: 10.0!')
+        #self.assertEqual(calcular_media(0, 0, 0), 'O aluno está reprovado com média: 0.0!')
+        #self.assertEqual(calcular_media(11, 5, 6), 'Média inválida!')
+
+    #def testar_ordenar_numeros_decrescente(self):
+        #self.assertEqual(ordenar_numeros_decrescente(5, 2, 8), [8, 5, 2])
+       # self.assertEqual(ordenar_numeros_decrescente(1, 1, 1), [1, 1, 1])
+       # self.assertEqual(ordenar_numeros_decrescente(-1, -5, -3), [-1, -3, -5])
+        #self.assertEqual(ordenar_numeros_decrescente(0, 10, 5), [10, 5, 0])
+
+       def testar_calcular_litros_usados(self):
+           self.assertAlmostEqual(calcular_litros_usados(2, 60), 10.0)  # 2 horas a 60 km/h = 120 km; 120/12 = 10 litros
+           self.assertAlmostEqual(calcular_litros_usados(1.5, 80), 10.0)  # 1.5 horas a 80 km/h = 120 km; 120/12 = 10 litros
+           self.assertAlmostEqual(calcular_litros_usados(3, 40), 10.0)   # 3 horas a 40 km/h = 120 km; 120/12 = 10 litros
 if __name__ == "__main__":
     unittest.main()
